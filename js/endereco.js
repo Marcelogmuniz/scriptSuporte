2ab6e79a97ae92d697a975f70d68fbc76ff5e31b
Marcelo Muniz <marcelogmuniz@outlook.com>
2023-09-09 20:58:08 -0300

    Create endereco.js

---
+++ js/endereco.js
@@ -0,0 +1,20 @@
+const enderecoCliente = document.getElementById("enderecoCliente");
+const enderecoAgendamento = document.getElementById("enderecoAgendamento");
+const enderecoContato = document.getElementById("enderecoContato");
+const enderecoAtual = document.getElementById("enderecoAtual");
+const enderecoNovo = document.getElementById("enderecoNovo");
+const enderecoReferencia = document.getElementById("enderecoReferencia");
+const enderecoMaps = document.getElementById("enderecoCliente");
+const enderecoOperador = document.getElementById("enderecoCliente");
+const osFormatada = `MUDANÇA DE ENDEREÇO
+
+CLIENTE: ${enderecoCliente}
+AGENDAMENTO: ${enderecoAgendamento}
+CONTATO ${enderecoContato}
+
+ENDEREÇO ATUAL: ${enderecoAtual}
+NOVO ENDEREÇO: ${enderecoNovo}
+REFERÊNCIA: ${enderecoReferencia}
+LOCALIZAÇÃO MAPS: ${enderecoMaps}
+
+OP: ${enderecoOperador}`
